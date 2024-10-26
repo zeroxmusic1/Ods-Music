28a8ab7b67ae9b77a7703b216d0d5dfa2d12c9a7
NOOB-CODER-RAJA <sudheerpal8257@gmail.com>
2024-10-26 14:37:30 +0530

    sᴏɴᴀʟɪ

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/sz8xwz.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/j0pqqb.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/9cevdg.jpg"
